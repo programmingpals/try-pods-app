002666be38d6e46f0dab892003bf852a3ed95200
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-04-19 19:49:05 -0700

    add addToList functionality

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -31,6 +31,7 @@ export default function Search() {
                     totalEpisodesCount
                     rssUrl
                     imageUrl
+                    datePublished
                 }
             }
         }
