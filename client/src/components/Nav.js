01588086d4c2fab7d22d28f2730103dd4e257e51
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-04-20 15:34:55 -0700

    add css and styling to search

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div>
-      <Link to={"profilepage/1"}>Click to view a profile page</Link>------
-      <Link to={"podcastlist/1"}>Click to view a podcast list page</Link>------
-      <Link to={"podcastresult"}>Click to view a query result page</Link>
-      <p></p>
+    <div class="nav">
+      <div class="nav-left">
+        <h4>Try-Pods</h4>
+      </div>
+      <div class="nav-right">
+        <p>Search</p>
+        <p>
+          <Link to={"profilepage/1"}>My Profile</Link>
+        </p>
+        <p>Rowan</p>
+      </div>
     </div>
   );
 }
+
+// <Link to={"podcastresult"}>Click to view a query result page</Link>
+// <Link to={"podcastlist/1"}>Click to view a podcast list page</Link>
