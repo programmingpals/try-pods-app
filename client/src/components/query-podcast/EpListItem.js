16903e86cab093285f88a9274ff6eddbc6e1ba8d
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-04-22 19:46:51 -0700

    shorten description length toggle

--- client/src/components/query-podcast/EpListItem.js
+++ client/src/components/query-podcast/EpListItem.js
@@ -1,25 +1,31 @@
 import React from "react";
-
+import { useState } from "react";
+import AnimateHeight from "react-animate-height";
 
 export default function EpListItem(props) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
+  const truncatedDescription = props.description.substring(0, 150) + "...";
 
   let seasonText = "";
   if (props.seasonNumber) {
-    seasonText = "Season " + `${props.seasonNumber}`
+    seasonText = "Season " + `${props.seasonNumber}`;
   }
 
   let time;
   const totalMinutes = Math.floor(props.duration / 60);
-  const seconds = (props.duration % 60).toString().padStart(2, '0');
+  const seconds = (props.duration % 60).toString().padStart(2, "0");
   const hours = Math.floor(totalMinutes / 60);
-  const minutes = (totalMinutes % 60).toString().padStart(2, '0');
-  if (hours === 0){
-    time = `${minutes}:${seconds}`
+  const minutes = (totalMinutes % 60).toString().padStart(2, "0");
+  if (hours === 0) {
+    time = `${minutes}:${seconds}`;
   } else {
-    time = `${hours}:${minutes}:${seconds}`
+    time = `${hours}:${minutes}:${seconds}`;
   }
-  
-
 
   const releaseFullDate = new Date(props.datePublished * 1000);
   const release = releaseFullDate.toDateString();
@@ -43,15 +49,21 @@ export default function EpListItem(props) {
           <p>Release date: {release}</p>
         </div>
         <div>
-        <audio controls>
-          <source src={`${props.audioUrl}`} type="audio/mpeg" />
+          <audio controls>
+            <source src={`${props.audioUrl}`} type="audio/mpeg" />
           </audio>
         </div>
         <div className="pod-list-item-description">
-          <p>{props.description}</p>
+          <AnimateHeight
+            height={"auto"}
+          >
+            <p>{showFullDescription ? props.description : truncatedDescription}</p>
+          </AnimateHeight>
+          <button onClick={toggleDescription}>
+            {showFullDescription ? "Show less" : "Show more"}
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
