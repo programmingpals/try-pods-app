41f2269a6c9437601b5059a2840afc97339ad86d
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-04-24 14:43:48 -0700

    wrap up page

---
+++ client/src/components/pages/WrapUp.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "client/src/assets/1Rowan.png"
+
+export default function WrapUp() {
+  return (
+    <div className="wrap-container">
+      <h2>Thats a wrap!</h2>
+
+      <div className="buds-container">
+        <div>
+          <h4>Kate Booton</h4>
+        </div>
+        <div>
+          <h4>Liz Fosdick</h4>
+        </div>
+        <div>
+          <h4>Rowan Mackenzie</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
